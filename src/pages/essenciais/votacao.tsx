116a4373a5b40de76a471ac3bb5db557e6732718
felipemrqs <felipe.marques.correia@gmail.com>
2024-12-18 23:23:28 -0300

    alteração realizada para que acontece concorrência de estado

--- src/pages/essenciais/votacao.tsx
+++ src/pages/essenciais/votacao.tsx
@@ -7,10 +7,10 @@ import Flex from "@/components/template/Flex";
 export default function () {
     const [votos, setVotos] = useState({naruto: 0, onePeace: 0});
     function votarNaruto() {
-        setVotos({...votos, naruto: votos.naruto + 1});
+        setVotos(prevVotos => ({...prevVotos, naruto: prevVotos.naruto + 1}));
     }
     function votarOnePeace() {
-        setVotos({...votos, onePeace: votos.onePeace + 1});
+        setVotos(prevVotos => ({...prevVotos, onePeace: prevVotos.onePeace + 1}));
     }
     return (
     <Pagina titulo="Votação" subtitulo="Usando estado com objeto">
